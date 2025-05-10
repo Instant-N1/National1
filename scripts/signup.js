353585eb0f6b540161a5591e5668d7c9a54a927b
Instant-N1 <noahgagneaud@gmail.com>
2025-05-10 14:41:02 +0200

    correction inscription

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,20 +1,21 @@
+// scripts/signup.js
 import { auth } from "./firebase-config.js";
-import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-auth.js";
 
+// Exemple d'inscription
 const signupForm = document.getElementById("signup-form");
+signupForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-document.getElementById('signup-form').addEventListener('submit', function(e) {
-  e.preventDefault();;
-
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+  const email = signupForm["email"].value;
+  const password = signupForm["password"].value;
 
   createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      alert("Compte créé avec succès !");
-      window.location.href = "../index.html"; // Redirection après inscription
+    .then((cred) => {
+      console.log("Utilisateur créé :", cred.user);
+      window.location.href = "mon-espace.html";
     })
-    .catch((error) => {
-      alert("Erreur : " + error.message);
+    .catch((err) => {
+      console.error("Erreur inscription :", err.message);
     });
 });
